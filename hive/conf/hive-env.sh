da203e28e5545f93660cc684f88a36b791516333
Rushikesh Jaisur <rushikeshjaisur11@gmail.com>
2022-10-30 12:07:50 +0530

    Hive metastore fixed

--- hive/conf/hive-env.sh
+++ hive/conf/hive-env.sh
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 # Set Hive and Hadoop environment variables here. These variables can be used
 # to control the execution of Hive. It should be used by admins to configure
 # the Hive installation (so that users do not have to set environment variables
@@ -21,12 +20,9 @@
 #
 # The hive service being invoked (CLI/HWI etc.) is available via the environment
 # variable SERVICE
-
-
 # Hive Client memory usage can be an issue if a large number of clients
 # are running at the same time. The flags below have been useful in 
 # reducing memory usage:
-#
 # if [ "$SERVICE" = "cli" ]; then
 #   if [ -z "$DEBUG" ]; then
 #     export HADOOP_OPTS="$HADOOP_OPTS -XX:NewRatio=12 -Xms10m -XX:MaxHeapFreeRatio=40 -XX:MinHeapFreeRatio=15 -XX:+UseParNewGC -XX:-UseGCOverheadLimit"
@@ -34,21 +30,14 @@
 #     export HADOOP_OPTS="$HADOOP_OPTS -XX:NewRatio=12 -Xms10m -XX:MaxHeapFreeRatio=40 -XX:MinHeapFreeRatio=15 -XX:-UseGCOverheadLimit"
 #   fi
 # fi
-
 # The heap size of the jvm stared by hive shell script can be controlled via:
-#
 # export HADOOP_HEAPSIZE=1024
-#
 # Larger heap size may be required when running queries over large number of files or partitions. 
 # By default hive shell scripts use a heap size of 256 (MB).  Larger heap size would also be 
 # appropriate for hive server (hwi etc).
-
-
 # Set HADOOP_HOME to point to a specific hadoop install directory
 # HADOOP_HOME=${bin}/../../hadoop
-
 # Hive Configuration Directory can be controlled by:
 # export HIVE_CONF_DIR=
-
 # Folder containing extra ibraries required for hive compilation/execution can be controlled by:
 # export HIVE_AUX_JARS_PATH=
