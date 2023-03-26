ef9bee510dc08f198020485c5421deb7a6f603fd
Rushikesh Jaisur <rushikeshjaisur11@gmail.com>
2023-03-27 00:18:57 +0530

    changes added

---
+++ airflow/dags/test_ssh.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.contrib.operators.ssh_operator import SSHOperator
+import datetime
+
+dag = DAG(dag_id="edgeNodeDAG", start_date=datetime.datetime.now())
+
+task1 = SSHOperator(
+    task_id="echo_task",
+    ssh_conn_id="edgenode",
+    command="whoami",
+    dag=dag
+)
+task2 = SSHOperator(task_id="spark_version",
+                    ssh_conn_id="edgenode",
+                    command="./$SPARK_HOME/bin/pyspark --verison",
+                    dag=dag)
